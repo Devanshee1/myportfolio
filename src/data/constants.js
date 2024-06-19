113f4a435bc5f934547a9feefdabfa963a0ec7f7
devanshee1 <devanshee7763@gmail.com>
2024-06-19 17:55:17 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,192 @@
+export const Bio = {
+    name: "Devanshee Sharma",
+    roles: [
+      "Front-end Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Devanshee1",
+    resume:
+      "https://drive.google.com/file/d/1k1Mh-FTv4BHsbDQZNv8Qg3s8CtmQJsZB/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/devanshee-sharma-0b7802200/",
+    twitter: "https://twitter.com/devanshee7763",
+    discord: "https://discord.com/devansheesharma_30997",
+    medium: "https://medium.com/@devanshee7763",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        
+        
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      role: "Team leader in SIH-2023",
+      company: "Smart India Hackathon",
+      date: "Aug 2023 - December 2023",
+      desc: "Leading a team at the Smart India Hackathon (SIH) was a transformative experience that showcased the power of hard work, determination, and team spirit. When we first embarked on this journey, we were quite naive. Our team consisted of enthusiastic but inexperienced members, all eager to make a mark but unsure of the path ahead.We spent countless hours brainstorming, prototyping, and testing our ideas. Each failure was a lesson, and each success was a motivation to push harder. We divided tasks based on individual strengths and ensured everyone was contributing to the best of their abilities which eventually led us to be the grand finalist.",
+    
+    },
+    {
+      id: 1,
+      role: "Student Placement Coordinator",
+      company: "BMSIT&M",
+      date: "February 2024 - Present",
+      desc: "Serving as a Student Placement Coordinator was a pivotal role that offered me a unique opportunity to bridge the gap between students and potential employers. This position allowed me to develop essential skills in coordination, communication, and leadership, while significantly contributing to the career prospects of my peers.",
+    },
+    {
+      id: 2,
+      role: "Vice Head corporate relations",
+      company: "Entrepreneurship cell at BMSIT&M",
+      date: "May 2023 - Present",
+      desc: "Serving as the Vice Head of the Entrepreneurship Cell (E-Cell) at BMSIT&M was an incredibly enriching experience that played a significant role in my personal and professional growth. This role allowed me to develop leadership skills, foster an entrepreneurial mindset, and contribute to the entrepreneurial ecosystem of our institution.",
+      
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      school: "Indian Institute of Technology,Madras(IITM)",
+      date: "2022-2026",
+      desc: "I am currently pursuing a Bachelor's degree in Data Science at IIT,madras.  I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bsc in Data Science & Applications",
+    },
+    {
+      id: 1,
+      
+      school: "BMS Institute of Technology & Management",
+      date: "2022-2026",
+      grade: "9.5 cgpa",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at BMS Institute of Technology & Management, Bangalore. I have completed 4 semesters and have a CGPA of 9.5. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am learning and working on exciting projects with a team of talented developers.",
+      degree: "B.E. in Computer Science",
+    },
+    {
+      id: 2,
+      school: "Maharshi Patanjali Vidya Mandir",
+      date: "2021",
+      grade: "95.6%",
+      desc: "I completed my class 12 education at MPVM school, Prayagraj, where I studied Science with Computer Application.",
+      degree: "CBSE(XII), Science with Computer",
+    },
+    {
+      id: 3,
+      school: "Maharshi Patanjali Vidya Mandir",
+      date: "2019",
+      grade: "95.6%",
+      desc: "I completed my class 10 education at MPVM School, Prayagraj.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Analytics based on Government Land Information System(GLIS) ",
+      date: "August 2023 - December 2023",
+      description:
+        "Working on the GLIS project was an enriching and challenging experience that significantly contributed to my technical skills and project management capabilities. This project aimed to develop a comprehensive system for managing geographic and land information, and it involved a blend of technical, analytical, and collaborative efforts.",
+      image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      category: "web app",
+        category: "web app",
+      github: "https://github.com/Visionaries-United/visionaries_unted_SIH_2023",
+    },
+    
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
